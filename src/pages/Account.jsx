cb04666cdf882ad408a37e32c8133e35195fb6e3
vsbron <vsbron@gmail.com>
2024-01-28 15:06:20 +0300

    Added option for users to update their password

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,4 +1,4 @@
-// import UpdatePasswordForm from "../features/authentication/UpdatePasswordForm";
+import UpdatePasswordForm from "../features/authentication/UpdatePasswordForm";
 import UpdateUserDataForm from "../features/authentication/UpdateUserDataForm";
 
 import Heading from "../ui/Heading";
@@ -16,7 +16,7 @@ function Account() {
 
       <Row>
         <Heading as="h3">Update password</Heading>
-        {/* <UpdatePasswordForm /> */}
+        <UpdatePasswordForm />
       </Row>
     </>
   );
